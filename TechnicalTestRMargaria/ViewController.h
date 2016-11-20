2fe780a1e9a7c1101be47f06aa0d551929b7107d
Remi Margaria <remi.margaria@pj.ca>
2016-11-20 15:41:13 -0500

    Initial Commit

---
+++ TechnicalTestRMargaria/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TechnicalTestRMargaria
+//
+//  Created by Remi Margaria on 16-11-20.
+//  Copyright © 2016 Margaria iOS Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TechnicalTestRMargaria/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TechnicalTestRMargaria
+//
+//  Created by Remi Margaria on 16-11-20.
+//  Copyright © 2016 Margaria iOS Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
